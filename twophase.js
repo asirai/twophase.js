157a20b7f9055b1c18bc7058ec285813b8a60724
asirai <asatoirifune@gmail.com>
2020-06-22 00:43:30 +0900

    Remove unnecessary code.

--- twophase.js
+++ twophase.js
@@ -935,8 +935,3 @@ return {
 }
 
 })();
-
-const tp = TWOPHASE.twophase;
-tp.initialize();
-tp.solve("D2 R U2 B' D' F U2 L' U R2 B D2 F B2 D2 L2 B'"); // solution: F D' R' B' U' F' B2 U' L D' F2 U' R2 F2 D' R2 B2 D' R2 D L2 D2
-tp.getScramble(347); // U R2 U' R2 D L2 D' F2 U2 B2 F2 L2 D' R' B L2 D' L2 U R' F D' L2
